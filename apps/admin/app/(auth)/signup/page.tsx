45b302ef455f7cc6b3a9aa6451ef6022a140e75f
coggiee <zentechie7@gmail.com>
2025-03-22 12:11:11 +0000

    style: 어드민 회원가입 정규식 및 라우터 추가

--- apps/admin/app/(auth)/signup/page.tsx
+++ apps/admin/app/(auth)/signup/page.tsx
@@ -1,102 +1,18 @@
-"use client";
+import { cn } from "@ui/lib/utils";
+import { checkUserAgent } from "../../../utils/check-user-agent";
+import SignupAuthSection from "../_components/signup-auth-section";
 
-import { Button } from "@uket/ui/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@uket/ui/components/ui/form";
-import { Input } from "@uket/ui/components/ui/input";
-import { useSignupForm } from "../../../hooks/use-signup-form";
+export default async function Page() {
+  const isMobileDevice = await checkUserAgent();
 
-export default function Page() {
-  const { form, onSubmit } = useSignupForm();
   return (
-    <section className="flex flex-col gap-11 m-[-1rem] p-[1rem] bg-[#E7DCF3]">
-      <main className="flex flex-col gap-10">
-        <h1 className="text-brand font-bold">
-          비밀번호를 설정하면 회원가입이 완료됩니다.
-        </h1>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col gap-5"
-          >
-            <div className="flex flex-col gap-3 w-full">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem className="grid w-full items-center gap-1.5 md:w-full">
-                    <FormLabel className="text-desc">ID</FormLabel>
-                    <FormControl>
-                      <Input
-                        id="id"
-                        placeholder="아이디"
-                        {...field}
-                        className="text-base"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem className="grid w-full items-center gap-1.5 md:w-full">
-                    <FormLabel className="text-desc">PW</FormLabel>
-                    <FormControl>
-                      <Input
-                        id="pw"
-                        type="password"
-                        placeholder="비밀번호"
-                        {...field}
-                        className="text-base"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="check_password"
-                render={({ field }) => (
-                  <FormItem className="grid w-full items-center gap-1.5 md:w-full">
-                    <FormLabel className="text-desc">PW</FormLabel>
-                    <FormControl>
-                      <Input
-                        id="check_pw"
-                        type="password"
-                        placeholder="비밀번호 확인"
-                        {...field}
-                        className="text-base"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="space-y-4">
-              <p className="h-5 text-center text-sm text-[#EF4444] sm:text-left">
-                에러
-              </p>
-              <Button
-                type="submit"
-                className="bg-brand hover:bg-brandHover w-full rounded-lg py-6 text-base md:min-w-80"
-              >
-                회원가입
-              </Button>
-            </div>
-          </form>
-        </Form>
-      </main>
+    <section
+      className={cn(
+        "flex flex-col gap-11 relative my-[-2rem] mx-[-3rem] py-4 px-8 bg-[#F0EDFD]",
+        !isMobileDevice && "rounded-xl",
+      )}
+    >
+      <SignupAuthSection />
     </section>
   );
 }
