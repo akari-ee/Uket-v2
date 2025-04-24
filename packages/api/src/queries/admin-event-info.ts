d8458c11a015d2d92deac63b1ae3e2cb3e228c6f
coggiee <zentechie7@gmail.com>
2025-04-24 17:10:28 +0000

    feat/UKET-103: 어드민 본인 정보 상세 조회 API 연결 및 그에 따른 부수 작업 (#38)

--- packages/api/src/queries/admin-event-info.ts
+++ packages/api/src/queries/admin-event-info.ts
@@ -1,7 +1,7 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { formatDate } from "@uket/util/time";
-import axios from "axios";
+import { fetcherAdmin } from "../admin-instance";
 import { AdminTicketInfoResponse } from "../types/admin-event";
 
 const DEFAULT_PAGE_NUMBER = 0;
@@ -11,9 +11,10 @@ const getAdminEventInfoList = async ({
   page = DEFAULT_PAGE_NUMBER,
   size = DEFAULT_PAGE_SIZE,
 }) => {
-  const { data } = await axios.get<AdminTicketInfoResponse>(
-    "/api/admin/event-info",
+  const { data } = await fetcherAdmin.get<AdminTicketInfoResponse>(
+    "/uket-event-registrations",
     {
+      mode: "BOUNDARY",
       params: {
         page,
         size,
