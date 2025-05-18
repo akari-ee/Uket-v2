794911ec39cff56b2ee0f0ef6e569e536a1ef655
coggiee <zentechie7@gmail.com>
2025-05-18 11:08:55 +0000

    fix: 긴급한 오류 수정

--- apps/web/app/(non-auth)/select-event/_components/uket-event-list.tsx
+++ apps/web/app/(non-auth)/select-event/_components/uket-event-list.tsx
@@ -25,7 +25,7 @@ export default function UketEventList({
             ({
               eventId,
               eventName,
-              eventThumbnailImagePath,
+              eventThumbnailImageId,
               eventStartDate,
               eventEndDate,
               ticketingStartDate,
