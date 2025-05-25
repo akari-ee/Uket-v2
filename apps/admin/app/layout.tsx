6542a04f68a16919a1f09d467578a663bfa172e7
coggiee <zentechie7@gmail.com>
2025-05-25 11:21:20 +0000

    Feat/uket 159: admin 메뉴 1:1 채팅, 카카오톡 채널 메뉴 추가 (#51)
    Co-authored-by: Hwisik Moon <zentechie7@gmail.com>

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -18,6 +18,12 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <script
+        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.5/kakao.min.js"
+        integrity="sha384-dok87au0gKqJdxs7msEdBPNnKSRT+/mhTVzq+qOhcL464zXwvcrpjeWvyj1kCdq6"
+        crossOrigin="anonymous"
+        async
+      ></script>
       <body className={`${notoSans.className}`}>
         <Providers>{children}</Providers>
         <Sonner richColors position="bottom-center" />
