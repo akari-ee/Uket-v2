36b4d289f996f2f83d843c10f0341514ceececc6
coggiee <zentechie7@gmail.com>
2025-07-09 04:16:24 +0000

    style/UKET-245: QA 티켓 처리 (#79)

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -6,6 +6,9 @@ import "@uket/ui/globals.css";
 import { notoSans } from "../config/fonts";
 import Providers from "./providers";
 
+import KakaoScript from "../utils/kakao";
+import GoogleAnalyticsScript from "@uket/util/google-analytics";
+
 export const metadata: Metadata = {
   title: "Uket for admin",
   description: "Uket 어드민을 위한 서비스입니다.",
@@ -18,16 +21,12 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <script
-        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.5/kakao.min.js"
-        integrity="sha384-dok87au0gKqJdxs7msEdBPNnKSRT+/mhTVzq+qOhcL464zXwvcrpjeWvyj1kCdq6"
-        crossOrigin="anonymous"
-        async
-      ></script>
       <body className={`${notoSans.className}`}>
         <Providers>{children}</Providers>
         <Sonner richColors position="bottom-center" />
       </body>
+      <GoogleAnalyticsScript />
+      <KakaoScript />
     </html>
   );
 }
