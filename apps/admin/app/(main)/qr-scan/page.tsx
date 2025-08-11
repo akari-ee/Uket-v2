d1a544e753ac1843019dd8e02c35177cbe9d3157
coggiee <zentechie7@gmail.com>
2025-08-11 04:54:01 +0000

    style: QR 스캔 페이지에 확대 불가능 기능 추가

--- apps/admin/app/(main)/qr-scan/page.tsx
+++ apps/admin/app/(main)/qr-scan/page.tsx
@@ -1,8 +1,17 @@
+import { Viewport } from "next";
 import QrScanner from "./_components/qr-scanner";
+
+export const viewport: Viewport = {
+  themeColor: "black",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
 export default function Page() {
   return (
-    <main className="flex h-full flex-col">
+    <main className="flex h-full flex-col touch-none">
       <section className="flex grow justify-center">
         <main className="relative h-full sm:max-w-fit">
           <QrScanner />
